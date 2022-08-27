f4d669d99de0eb4dc7fab50f437353cd13a54339
AmoghGS <79263891+AmoghGS@users.noreply.github.com>
2022-08-27 12:21:22 +0530

    Add files via upload

---
+++ src/components/footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './footer.css';
+
+const footer = () => {
+  return (
+    <>
+      <div className='footer'>
+        <div className="details" id="d1">
+          <p>Amogh G S<br/>
+          B.Tech(CSE) III sem<br/>
+          amoghgs123@gmail.com</p>
+        </div>
+        <div className="details" id="d2">
+          <p>Aniruddha Kulkarni<br/>
+          B.Tech(CSE) III sem<br/>
+          aniruddha.k02@gmail.com</p>
+        </div>
+        <div className="details" id="d3">
+          <p>Abhishek Patil<br/>
+          B.Tech(CSE) III sem<br/>
+          abhijpatil2003@gmail.com</p>
+        </div>
+      </div>
+
+    </>
+  )
+}
+export default footer
